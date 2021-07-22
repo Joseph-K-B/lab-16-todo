7736dafbd500e9caf1c612eb724064ab142d45a0
Joseph-K-B <303josephb@gmail.com>
2021-07-22 10:59:23 -0700

    passing createUser function

--- test/tests.js
+++ test/tests.js
@@ -1,6 +1,8 @@
 // IMPORT MODULES under test here:
 // import { add } from '../calculator.js';
 
+import { createUser } from '../user/create-user.js';
+
 const test = QUnit.test;
 
 // name your test by what it is testing
@@ -16,4 +18,22 @@ test('time to test a function', (expect) => {
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+});
+
+
+test('should create new user', (expect) => {
+    const expected = {
+        username: 'planty12',
+        password: '1234',
+        toDos: [
+            {
+                todo: '',
+                completed: false
+            },
+        ] };
+    const userData = new FormData();
+    userData.set('username', 'planty12');
+    userData.set('password', 1234);
+    const actual = createUser(userData);
+    expect.deepEqual(actual, expected);
 });
---
+++ user/create-user.js
@@ -0,0 +1,11 @@
+export function createUser(formData){
+    const newUser = {
+        username: formData.get('username'),
+        password: formData.get('password'),
+        toDos: [{
+            todo:'',
+            completed: false
+        }],
+    };
+    return newUser;
+}
