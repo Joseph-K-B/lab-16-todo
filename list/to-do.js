fdbee3284e19d942b64c5dea404c76134b9355f9
Cristian Montes <cmontesmujica@gmail.com>
2021-07-22 19:42:25 -0700

    added board, fix username password and cross dom element and working on changing completed to true

--- list/to-do.js
+++ list/to-do.js
@@ -5,9 +5,9 @@ import { getUser, saveUser } from '../data/data-function.js';
 const list = document.getElementById('to-do-list');
 const addBtn = document.getElementById('to-do-add');
 const input = document.getElementById('to-do');
+const ul = document.querySelector('ul');
 
 const user = getUser();
-// const toDoLocal 
 
 
 addBtn.addEventListener('click', (e)=>{
@@ -19,7 +19,15 @@ addBtn.addEventListener('click', (e)=>{
     list.append(li);
     input.value = '';
 });
+
 const a = document.createElement('a');
 a.textContent = 'home'; 
 list.append(a);
 a.href = '../index.html';
+
+ul.addEventListener('click', (e)=> {
+    if (e.target.tagName === 'LI'){
+        e.target.classList.add('done');
+    }
+  
+});
