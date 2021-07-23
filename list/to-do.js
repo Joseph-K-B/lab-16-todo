303730577d2b9a6435768ebcc8198aa46010f32c
Cristian Montes <cmontesmujica@gmail.com>
2021-07-23 11:35:57 -0700

    display items for user if log back in and  and event listener  to complete todos

--- list/to-do.js
+++ list/to-do.js
@@ -1,5 +1,4 @@
 import { getUser, saveUser } from '../data/data-function.js';
-// import users from '../data/user-data.js';
 
 
 const list = document.getElementById('to-do-list');
@@ -10,6 +9,7 @@ const ul = document.querySelector('ul');
 const user = getUser();
 
 
+//ADD ITEMS TO THE LIST EVENT LISTENER
 addBtn.addEventListener('click', (e)=>{
     user.toDos.push({ todo:input.value, completed: false });
     saveUser(user);
@@ -20,14 +20,39 @@ addBtn.addEventListener('click', (e)=>{
     input.value = '';
 });
 
+//GO HOME LINK
 const a = document.createElement('a');
 a.textContent = 'home'; 
 list.append(a);
 a.href = '../index.html';
 
+// ul.addEventListener('click', (e)=> {
+//     if (e.target.tagName === 'LI'){
+//         e.target.classList.add('done'); 
+//     }
+//     saveUser(user);
+// });
+
+//TO DISPLAY EXISTING ITEM IF USER LOGS IN AGAIN
+if (user){
+    for (let item of user.toDos){
+        const li = document.createElement('li');
+        li.innerText = item.todo;
+        list.append(li);
+    }
+}
+
+
+// IDEA TO MAKE THE FALSE GO TO TRUE... NEED TO CHANGE THE TARGET TO BUTTONS FOR IT TO WORK AND MATCH APPLES TO APPLES
 ul.addEventListener('click', (e)=> {
     if (e.target.tagName === 'LI'){
-        e.target.classList.add('done');
+        e.target.classList.add('done'); 
+        for (let item of user.toDos){
+            if (e.target.textContent === item.todo){
+                item.completed = true;
+            }
+        }
+           
     }
-  
+    saveUser(user);
 });
